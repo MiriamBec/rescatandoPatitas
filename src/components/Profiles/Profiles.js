68068711f67581d4d316dbb2cc16eb0f156bd2b6
miribec <miriam8894bec@gmail.com>
2023-09-14 14:12:11 -0700

    Se ha iniciado con la página de los perfiles

---
+++ src/components/Profiles/Profiles.js
@@ -0,0 +1,58 @@
+import "./Profiles.css";
+
+const Profiles = () => {
+  return (
+    <section className="profiles">
+      <h1>Rescatados</h1>
+      <div className="div-content-profiles">
+        <div className="div-profiles">
+          <img src="/img/dog1.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog2.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog3.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog4.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog5.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog1.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog2.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog3.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog4.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog5.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog1.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog2.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog3.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog4.jpg" alt="dog1"></img>
+        </div>
+        <div className="div-profiles">
+          <img src="/img/dog5.jpg" alt="dog1"></img>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Profiles;
